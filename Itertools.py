78c78f26aee90b59957012b5b9b5d813d909f5c4
vpc <cuevasvp@gmail.com>
2021-04-06 09:34:20 +0800

    refactor

--- Itertools.py
+++ Itertools.py
@@ -1,6 +1,11 @@
 from itertools import permutations, combinations, combinations_with_replacement, product
 
-print(list([''.join(comb) for comb in permutations('abc', 2)]))
-print(list([''.join(comb) for comb in combinations('abc', 2)]))
-print(list([''.join(comb) for comb in combinations_with_replacement('abc', 2)]))
-print(list([''.join(prod) for prod in product('abc', repeat=2)]))
+print([''.join(perm) for perm in permutations('abc', 2)])
+print([''.join(comb) for comb in combinations('abc', 2)])
+print([''.join(comb) for comb in combinations_with_replacement('abc', 2)])
+print([''.join(prod) for prod in product('abc', repeat=2)])
+
+print(tuple(''.join(perm) for perm in permutations('abc', 2)))
+print(tuple(''.join(comb) for comb in combinations('abc', 2)))
+print(tuple(''.join(comb) for comb in combinations_with_replacement('abc', 2)))
+print(tuple(''.join(prod) for prod in product('abc', repeat=2)))
