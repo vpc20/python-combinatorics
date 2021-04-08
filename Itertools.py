ff51e1488403f6cf2ab1dcac027fa325f6f702be
vpc <cuevasvp@gmail.com>
2021-04-08 17:30:28 +0800

    additional examples

--- Itertools.py
+++ Itertools.py
@@ -9,3 +9,6 @@ print(tuple(''.join(perm) for perm in permutations('abc', 2)))
 print(tuple(''.join(comb) for comb in combinations('abc', 2)))
 print(tuple(''.join(comb) for comb in combinations_with_replacement('abc', 2)))
 print(tuple(''.join(prod) for prod in product('abc', repeat=2)))
+
+print(tuple(''.join(prod) for prod in product('abc', 'def')))
+print(tuple(''.join(prod) for prod in product(*['abc', 'def'])))
